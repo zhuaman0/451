77a24e8d56f08d8f9a7e8d223d3719f191784a5f
Jomart7 <gamecsgo013@gmail.com>
2024-10-31 12:47:40 +0500

    finished adminQuiz

--- pages/adminAnswer.vue
+++ pages/adminAnswer.vue
@@ -1,9 +1,174 @@
 <script setup>
 definePageMeta({
-    layout: 'main'
+    layout: 'small'
 })
 </script>
 
 <template>
+    <div class="body">
+        <div class="link">
+            <div >Questions</div>
+            <div class="href">Answers</div>
+        </div>
+        <div class="container">
+    <h1>Question 1</h1>
+    <table>
+        <tr class="first">
+            <th>Index</th>
+            <th>Team name</th>
+            <th>Submit Time</th>
+            <th>Answer</th>
+            <th>Answer</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Kuka x2</td>
+            <td>12.10.2024 <span class="highlighted">13:15</span></td>
+            <td>mal</td>
+            <td>mal</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pythons</td>
+            <td></td>
+            <td>kniga</td>
+            <td>kniga</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Leaders</td>
+            <td></td>
+            <td>aman</td>
+            <td>aman</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Leaders</td>
+            <td></td>
+            <td>kaany</td>
+            <td>kaany</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Leaders</td>
+            <td></td>
+            <td>kaneke</td>
+            <td>kaneke</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Leaders</td>
+            <td></td>
+            <td>zhomeke</td>
+            <td>zhomeke</td>
+        </tr>
+        
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+
+                <button class="edit-button">Edit</button>
+            </td>
+        </tr>
+    </table>
     
+</div>
+    </div>
 </template>
+<style scoped>
+.body{
+    color: white;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    row-gap: 20px;
+}
+
+.link{
+    margin-top: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width:260px;
+    height: 32px;
+    background-color: #333333;
+    color: white;
+    border-radius: 50px;
+    gap: 13px;
+    transition: all 0.2s;
+}
+
+.href{
+    width: 130px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #777777;
+    border-radius: 50px;
+}
+
+.link div{
+    font-size: 19px;
+}
+
+.first{
+    background-color: #444444;
+}
+
+.first th{
+    color: white;
+}
+
+.container {
+    text-align: center;
+    width: 80%;
+}
+h1 {
+    font-size: 2rem;
+    margin-bottom: 20px;
+}
+table {
+    width: 100%;
+    border-collapse: collapse;
+    background-color: #282828;
+    color: #ffffff;
+    border: 2px solid #666;
+    border-radius: 18px;
+    overflow: hidden;
+}
+th, td {
+    padding: 15px;
+    text-align: center;
+    font-size: 1rem;
+    border-bottom: 1px solid #333;
+    color: white;
+}
+th {
+    font-weight: normal;
+    color: #b2b2b2;
+}
+td {
+    color: #ffffff;
+}
+.highlighted {
+    color: #ff8c42;
+}
+.edit-button {
+    display: inline-block;
+    margin-top: 20px;
+    padding: 8px 40px;
+    background-color: #F29D00;
+    color: #ffffff;
+    border: none;
+    border-radius: 50px;
+    font-size: 1rem;
+    cursor: pointer;
+    text-decoration: none;
+}
+
+</style>
--- pages/adminQuiz.vue
+++ pages/adminQuiz.vue
@@ -1,9 +1,270 @@
 <script setup>
 definePageMeta({
-    layout: 'main'
+    layout: 'small'
 })
 </script>
 
 <template>
-    
+    <div class="body">
+        <section class="game">
+            <div class="link">
+                <div class="href">Questions</div>
+                <div @click="$emit('changePage', 'Page2')">Answers</div>
+            </div>
+            <input type="text" class="main-text" placeholder="Type your question"/>
+            <main class="question">
+                <button class="btn">+</button>
+                <p class="txt">Find and insert media</p>
+                <label for="file-upload" class="upload-btn">
+                    <span class="not-same">Upload file</span> or drag here to upload
+                    <input type="file" id="file-upload" hidden>
+                </label>
+            </main>
+            <main class="variants">
+                <div class="container">
+                    <div class="content">
+                        <div class="color"></div>
+                        <input type="text" placeholder="Text Field"/>
+                    </div>
+                    
+                </div>
+            </main>
+        </section>  
+        <div class="linee"></div>
+        <section class="quizz">
+            <div  class="prepares">
+                <h3>Enter QUIZ title</h3>
+                <input type="text" placeholder="Title">
+            </div>
+            <div class="prepares">
+                <h3>Time Limit</h3>
+                <input type="number" placeholder="20 seconds">
+            </div>
+            <div class="prepares">
+                <h3>Question type</h3>
+                <select>
+                    <option value="quiz">Quiz</option>
+                    <option value="type_answer">Type Answer</option>
+                </select>
+            </div>
+            <div class="prepares">
+                <h3>Points</h3>
+                <input type="number" placeholder="1">
+            </div>
+            <button class="button">Submit</button>
+        </section>
+    </div>
 </template>
+<style scoped>
+.body{
+    color: white;
+    width: 100%;
+    display: flex;
+}
+
+.link{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width:250px;
+    height: 32px;
+    background-color: #333333;
+    color: white;
+    border-radius: 50px;
+    gap: 13px;
+    transition: all 0.2s;
+}
+
+.href{
+    width: 130px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #777777;
+    border-radius: 50px;
+}
+
+.link div{
+    font-size: 19px;
+    cursor: pointer;
+}
+
+.main-text{
+    width: 500px;
+    margin: 0 auto;
+    text-align: center;
+    font-size: 38px;
+    background-color: transparent;
+    color: white;
+    font-weight: 600;
+    border: none;
+}
+
+.main-text::placeholder{
+    color: white;
+    font-size: 42px;
+    font-weight: 700;
+}
+
+.main-text:focus{
+    outline: none;
+}
+
+.linee{
+    border-right: 1px solid #444444;
+    height: 89vh;
+}
+
+.game{
+    width: 73%;
+    height: 100%;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    row-gap: 50px;
+    margin-top: 30px;
+}
+
+.question{
+    width: 450px;
+    background-color: #444444;
+    height: 200px;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    margin: 0 auto
+}
+
+.btn{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 40px;
+    background-color: #555555;
+    border: none;
+    color: white;
+    font-size: 60px;
+    font-weight: 300;
+    cursor: pointer;
+}
+
+.not-same{
+    font-weight: 700;
+    border-bottom: 1.4px solid;
+}
+
+.upload-btn{
+    cursor: pointer;
+    font-size: 18px;
+}
+
+.txt{
+    font-size: 18px;
+}
+
+.variants{
+    width: 750px;
+    background-color: transparent;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    row-gap: 20px;
+}
+
+.container{
+    display: flex;
+    justify-content: center;
+}
+
+.content{
+    width: 560px;
+    height: 80px;
+    background-color: #333333;
+    border-radius:15px;
+    display: flex;
+    align-items: center;
+}
+
+.content input{
+    margin-left:30px;
+    width: 500px;
+    margin: 0 auto;
+    font-size: 26px;
+    background-color: transparent;
+    color: rgb(227, 227, 227);
+    font-weight: 600;
+    border: none;
+}
+
+.content input::placeholder{
+    color: rgb(227, 227, 227);
+    font-size: 24px
+}
+
+.content input:focus{
+    outline: none;
+}
+
+.color{
+    border-radius:10px;
+    height: 90%;
+    width: 24px;
+    margin-left: 6px;
+    background-color: #F69056;
+}
+
+.prepares h3{
+    font-weight:500; 
+}
+
+.prepares input{
+    border: 2px solid #ED7B1D;
+    background-color: transparent;
+    height: 34px;
+    width: 100%;
+    border-radius: 8px;
+    padding: 0 5px;
+    font-size: 16px;
+    color: white;
+}
+
+.prepares select{
+    border: 2px solid #ED7B1D;
+    background-color: transparent;
+    height: 34px;
+    width: 100%;
+    border-radius: 8px;
+    padding: 0 5px;
+    font-size: 16px;
+    color: #777777;
+}
+
+.button{
+    display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 35px;
+        border-radius: 8px;
+        border: none;
+        font-weight: 700;
+        font-size: 16px;
+        color: white;
+        background: linear-gradient(45deg, #ff6600, #ffcc33);
+        cursor: pointer;
+}
+
+.quizz{
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    margin-top: 30px;
+}
+</style>
